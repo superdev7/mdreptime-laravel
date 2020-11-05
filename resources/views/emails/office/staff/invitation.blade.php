11475dce8f655590f8dbf0851b64bc6229ef8d65
SuperDev <superdev7@gmail.com>
2020-11-04 20:08:25 -0800

    updated inviation emails

--- resources/views/emails/office/staff/invitation.blade.php
+++ resources/views/emails/office/staff/invitation.blade.php
@@ -1,12 +1,13 @@
 @component('mail::message')
-# Introduction
+# Hi {{ $guestUser->first_name }},
 
-The body of your message.
+{{ $office->label }} {{ __('invites you to join MD REP TIME, a website designed to simplify your day to day interactions with industry representatives') }} .
+{{ __('We are revolutionizing the way reps interact with medical practices by providing you with a digital platform to manage meetings and communications') }}.
 
-@component('mail::button', ['url' => ''])
-Button Text
+{{ __('Please click the link below and follow the registration process') }}:
+@component('mail::button', ['url' => route('invitation.show', ['invite_code' => $guestUser->invite_code])])
+{{ __('Click Here') }}
 @endcomponent
-
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
