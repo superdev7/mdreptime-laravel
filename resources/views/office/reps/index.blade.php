6ce500e57d15b6310a697c73699539f287728e23
SuperDev <superdev7@gmail.com>
2021-01-12 19:16:40 -0800

    continous app development changes

--- resources/views/office/reps/index.blade.php
+++ resources/views/office/reps/index.blade.php
@@ -19,7 +19,48 @@
                         <ul class="list-group">
                             @foreach($reps as $rep)
                                 <li class="list-group-item">
-
+                                    @component('components.elements.link', [
+                                        'href'  => '#'
+                                    ])
+                                        <div class="row">
+                                            <div class="col-12 col-md-2">
+                                                <div class="user-avator image">
+                                                    <img src="{{ avator($rep) }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-md-10">
+                                                <h5>{{ $rep->first_name }} {{ $rep->last_name }}</h5>
+                                                <h6 class="font-weight-normal">{{ __('Place holder for RX Data') }}</h6>
+                                            </div>
+                                        </div>
+                                        <div class="row mt-1">
+                                            <div class="col-12">
+                                                <ul class="list-unstyled">
+                                                    <li class="d-inline-block mr-3">
+                                                        @component('components.elements.link', [
+                                                            'href'  => '#',
+                                                        ])
+                                                            <i class="far fa-check-square"></i> {{ __('Approve') }}
+                                                        @endcomponent
+                                                    </li>
+                                                    <li class="d-inline-block mr-3">
+                                                        @component('components.elements.link', [
+                                                            'href'  => '#',
+                                                        ])
+                                                            <i class="far fa-heart"></i> {{ __('Favorite') }}
+                                                        @endcomponent
+                                                    </li>
+                                                    <li class="d-inline-block mr-3">
+                                                        @component('components.elements.link', [
+                                                            'href'  => '#',
+                                                        ])
+                                                            <i class="fas fa-ban"></i> {{ __('Block') }}
+                                                        @endcomponent
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    @endcomponent
                                 </li>
                             @endforeach
                         </ul>
